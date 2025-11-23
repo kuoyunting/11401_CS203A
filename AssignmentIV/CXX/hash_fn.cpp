8af73f99ae301078be55b37f84060fa31357028a
kuoyunting <willa48694062@gmail.com>
2025-11-24 03:04:26 +0800

    Update hash_fn.cpp

--- AssignmentIV/CXX/hash_fn.cpp
+++ AssignmentIV/CXX/hash_fn.cpp
@@ -1,9 +1,12 @@
-int myHashInt(int key, int m) {
-    return key % m;
-}
-
 int myHashString(const std::string& str, int m) {
-    int sum = 0;
-    for(char c : str) sum += c;
-    return sum % m;
+    if (m <= 0) return 0;
+
+    unsigned long hash = 0;
+    int p = 31;
+
+    for (char c : str) {
+        hash = (hash * p + (unsigned char)c) % m;
+    }
+
+    return (int)hash;
 }
